1a5463e80dfb4d50a2de39de66cb1fa682fd25a1
root <root@h1824709.stratoserver.net>
2011-07-18 19:57:12 +0200

    Typo in cv korrigiert. Javascript repariert (jQuery) für Rails funktioniert nun..

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module NeumannBiz
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(https://apis.google.com/js/plusone.js http://platform.twitter.com/widgets.js)
+    config.action_view.javascript_expansions[:defaults] = %w(https://apis.google.com/js/plusone.js http://platform.twitter.com/widgets.js https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js  rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
